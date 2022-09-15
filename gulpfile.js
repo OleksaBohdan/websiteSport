22b9a87f4a58dd252e4b87b2c223bc4fca2d8fe1
OleksaBohdan <bohdan.oleksa@gmail.com>
2022-09-15 12:32:16 +0200

    added gulp-rename

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass')(require('sass'));
+const rename = require('gulp-rename');
 
 gulp.task('server', function () {
   browserSync.init({
@@ -16,6 +17,7 @@ gulp.task('styles', function () {
   return gulp
     .src('src/sass/**/*.+(scss|sass)')
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min', prefix: '' }))
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.stream());
 });
